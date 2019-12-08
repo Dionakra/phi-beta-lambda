7f8029d32c8c7c17949c22730b132d4c0b303f72
David de los Santos Boix <ddlsb@hotmail.com>
2019-12-08 16:41:30 +0000

    Added QueryString for Culture Categories

--- pages/cultura.vue
+++ pages/cultura.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="mx-auto">
-    <div class="md:w-full lg:w-1/2 mx-auto flex flex-wrap justify-between  text-sm mb-3">
-      <div v-for="category in Object.keys(culture).sort((a, b) => a.localeCompare(b))" :key="category">
-        <button
+    <div class="md:w-full lg:w-1/2 mx-auto flex flex-wrap justify-between text-sm mb-3">
+      <div
+        v-for="category in Object.keys(culture).sort((a, b) => a.localeCompare(b))"
+        :key="category"
+      >
+        <nuxt-link
+          :to="{ path: '/cultura', query: { cat: category }}"
+          tag="button"
           class="hover:bg-red text-white py-1 px-2 rounded-full"
           :class="selectedCategory == category ? 'bg-red' : 'bg-transparent text-red border border-red hover:text-white'"
-          @click="
-            selectedCategory = category;
-            updateList();
-          "
-        >{{trans[category]}}</button>
+        >{{trans[category]}}</nuxt-link>
       </div>
     </div>
 
@@ -25,26 +26,31 @@
           :key="culture.id"
           class="h-auto w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 mb-2 mt-6 relative"
         >
-        <Culture :culture="culture" />
+          <Culture :culture="culture" />
+        </div>
       </div>
-    </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import Culture from "~/components/Culture"
+import Culture from "~/components/Culture";
 const PAGE = 30;
 
 export default {
   components: {
     Culture
   },
-  async asyncData() {
-    return axios.get("/api/culture.json").then(x => {
-      return { culture: x.data  };
-    });
+  watchQuery: ['cat'],
+  key: to => to.fullPath,
+  async asyncData({ query }) {
+    console.log(query.cat || "comedy-special");
+    const res = await axios.get("/api/culture.json");
+    return {
+      culture: res.data,
+      selectedCategory: query.cat || "comedy-special"
+    };
   },
   data() {
     return {
@@ -52,12 +58,12 @@ export default {
       searching: true,
       curPage: 1,
       showing: [],
-      selectedCategory: "comedy-special",
+      selectedCategory: "",
       trans: {
-        "documentary": "Documentales",
+        documentary: "Documentales",
         "comedy-special": "Especiales de comedia",
-        "book": "Libros",
-        "movie": "Películas",
+        book: "Libros",
+        movie: "Películas",
         "tv-show": "Programa TV",
         "radio-show": "Radio / Podcast",
         "tv-show-series": "Series"
@@ -70,7 +76,7 @@ export default {
   methods: {
     showCulture() {
       this.searching = true;
-      const res = this.culture[this.selectedCategory]
+      const res = this.culture[this.selectedCategory];
       this.searching = false;
       return res;
     },
